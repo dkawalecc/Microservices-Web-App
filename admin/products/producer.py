809ace836dee519fd16f9cd5278bfc16c1154544
dkawalecc <dkawalec08@gmail.com>
2021-08-17 21:55:39 +0200

    admin producer link revised

--- admin/products/producer.py
+++ admin/products/producer.py
@@ -1,7 +1,8 @@
 import pika
 
 # private mqqt key
-url_params = open('url_parameters.txt').read()
+url_params = open('/app/products/url_parameters.txt').read()
+# print(url_params)
 params = pika.URLParameters(url_params)
 
 connection = pika.BlockingConnection(params)
@@ -10,4 +11,5 @@ channel = connection.channel()
 
 
 def publish(method, body):
-    channel.basic_publish(exchange='', routing_key='admin', body='hello')
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='admin', body='hello main', properties=properties)
